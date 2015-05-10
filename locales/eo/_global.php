a14f74db830beee20d00c8fc8c41c59ef7ee5334
Keith Bowes <zooplah@gmail.com>
2015-05-09 23:31:27 -0400

    Allow country-agnostic language codes

--- locales/eo/_global.php
+++ locales/eo/_global.php
@@ -5,7 +5,7 @@
  */
 if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.' );
 
-$trans['eo_EO'] = array(
+$trans['eo'] = array(
 '__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
 '' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2015-04-20 18:15-0400\nPO-Revision-Date: 2015-04-20 18:16-0400\nLast-Translator: Keith Bowes <keith@localhost>\nLanguage-Team: Esperanto\nLanguage: \nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
 'Implements Pavatar support.' => 'Efektivigas regon de Pavatar.',
