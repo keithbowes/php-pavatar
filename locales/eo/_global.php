7cc84732eb5566f12dfd1490af43128245d208e2
Keith Bowes <keithbowes@users.noreply.github.com>
2018-06-12 16:05:12 -0400

    0.5

--- locales/eo/_global.php
+++ locales/eo/_global.php
@@ -7,7 +7,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
 
 $trans['eo'] = array(
 '__meta__' => array('format_version'=>1, 'charset'=>'utf-8'),
-'' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2017-02-20 22:57-0500\nPO-Revision-Date: 2017-02-20 22:59-0500\nLast-Translator: Keith Bowes <keith@localhost>\nLanguage-Team: Esperanto\nLanguage: eo\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
+'' => "Project-Id-Version: b 2e\nReport-Msgid-Bugs-To: \nPOT-Creation-Date: 2018-06-12 16:04-0400\nPO-Revision-Date: 2018-06-12 16:04-0400\nLast-Translator: Keith Bowes <keith@localhost>\nLanguage-Team: Esperanto\nLanguage: eo\nMIME-Version: 1.0\nContent-Type: text/plain; charset=UTF-8\nContent-Transfer-Encoding: 8bit\nPlural-Forms: nplurals=2; plural=(n != 1);\n",
 'Implements Pavatar support.' => 'Efektivigas regon de Pavatar.',
 'Displays Pavatars in your entries and comments without having to mess around with PHP.' => 'Montras Pavatar-ojn en viaj blogeroj kaj komentoj sen la bezono de PHP-programado.',
 'This plugin requires the PHP curl extension' => 'La kromprogramo postulas la PHP-etandaĵon curl',
